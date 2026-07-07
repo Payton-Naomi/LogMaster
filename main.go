844b0d1e27942d09ded583a1dc02f3e66f135b77
wwwzy-666 <2770464811@qq.com>
2026-07-07 19:05:25 +0800

    add main.go

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
-package main
+﻿package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-
-	"logmaster-agent/agent"
-	"logmaster-agent/agent/config"
-)
+import "fmt"
 
 func main() {
-	configPath := flag.String("config", "agent/config.yaml", "Path to config file")
-	flag.Parse()
-
-	cfg, err := config.Load(*configPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
-		os.Exit(1)
-	}
-
-	ag := agent.New(cfg)
-	if err := ag.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Agent error: %v\n", err)
-		os.Exit(1)
-	}
+    fmt.Println("Hello, World!")
 }
